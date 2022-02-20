e996fcdc9b20e846a646da249b59c3c109151834
iamzin <eunzin.park@gmail.com>
2022-02-20 14:35:38 +0900

    chore: initial

---
+++ src/main/java/com/flab/interoffice/InterofficeApplication.java
@@ -0,0 +1,13 @@
+package com.flab.interoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterofficeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InterofficeApplication.class, args);
+  }
+
+}
