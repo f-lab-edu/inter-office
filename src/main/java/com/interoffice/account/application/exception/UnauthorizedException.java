21bef8faac12bade02c2ed3add11341cabde7fb6
jws1837 <jws1837@gmail.com>
2022-06-23 20:43:45 +0900

    feat: UnauthorizedException

---
+++ src/main/java/com/interoffice/account/application/exception/UnauthorizedException.java
@@ -0,0 +1,10 @@
+package com.interoffice.account.application.exception;
+
+public class UnauthorizedException extends RuntimeException {
+
+  public UnauthorizedException(String s) {
+    super(s);
+  }
+}
+
+
